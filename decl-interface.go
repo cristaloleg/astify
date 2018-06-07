52f68b6093e5f7f1a7874182e2d895a43223f0bc
Oleg Kovalov <oleg.kovalov@allegro.pl>
2018-06-07 18:05:07 +0200

    decl: interface

---
+++ decl-interface.go
@@ -0,0 +1,33 @@
+package astify
+
+import "go/ast"
+
+// Interface ...
+type Interface struct {
+	declaration
+	name    string
+	methods []*Function
+}
+
+func newInterface(spec *ast.TypeSpec) *Interface {
+	iface := &Interface{
+		name: spec.Name.Name,
+	}
+
+	astIface := spec.Type.(*ast.InterfaceType)
+	for _, m := range astIface.Methods.List {
+		fn := newMethod(m)
+		iface.methods = append(iface.methods, fn)
+	}
+	return iface
+}
+
+// Name ...
+func (i *Interface) Name() string {
+	return i.name
+}
+
+// Methods ...
+func (i *Interface) Methods() []*Function {
+	return i.methods
+}
