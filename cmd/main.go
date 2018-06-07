7e79ffa0619729cd1b1f434be0513dcb2cc98acd
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-06-07 18:30:05 +0200

    add: main.go

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cristaloleg/astify"
+)
+
+func main() {
+	a, err := astify.Parse("~/go/src/github.com/cristaloleg/astify")
+	if err != nil {
+		panic(err)
+	}
+
+	a.Walk(func(f *astify.GoFile, n astify.Node) error {
+		switch {
+		case astify.IsStruct(n):
+			strct := astify.AsStruct(n)
+			fmt.Printf("# %v : %v \n", strct.Name(), len(strct.Fields()))
+
+		case astify.IsInterface(n):
+			iface := astify.AsInterface(n)
+			for _, m := range iface.Methods() {
+				fmt.Printf("# %v %v \n", m.Name(), len(m.Params()))
+			}
+
+		case astify.IsFunction(n):
+			fn := astify.AsFunction(n)
+			if !fn.IsMethod() && !fn.IsExported() {
+				fmt.Printf("$ %v %v \n", fn.Name(), len(fn.Results()))
+			}
+		}
+
+		return nil
+	})
+}
