dec9d84846f48b19244164daf1ada54b39cd3cac
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-06-09 17:14:46 +0200

    stmt: defer

--- stmt-defer.go
+++ stmt-defer.go
@@ -1,11 +1,13 @@
 package astify
 
+import "go/ast"
+
 // Defer ...
 type Defer struct {
 	statement
 }
 
-func newDefer(...interface{}) *Defer {
+func newDefer(*ast.CallExpr) *Defer {
 	g := &Defer{}
 	return g
 }
