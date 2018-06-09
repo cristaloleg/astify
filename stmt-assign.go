a47c6de3ea434b826e5f9e7c9c789f2a873492ed
Oleg Kovalov <iamolegkovalov@gmail.com>
2018-06-09 17:15:18 +0200

    stmt: assign

--- stmt-assign.go
+++ stmt-assign.go
@@ -5,12 +5,28 @@ import "go/ast"
 // Assign ...
 type Assign struct {
 	statement
-	dst        Value
-	src        Value
+	dst        []*Value
+	src        []*Value
 	isCreation bool
 }
 
-func newAssign(ast.Expr) *Assign {
-	a := &Assign{}
+func newAssign(dst, src []ast.Expr) *Assign {
+	a := &Assign{
+		dst: make([]*Value, 0, len(dst)),
+		src: make([]*Value, 0, len(src)),
+	}
+	for _, d := range dst {
+		_ = d
+		// a.dst = append(a.dst, newValue(nil))
+	}
+	for _, s := range src {
+		_ = s
+		// a.src = append(a.src, newValue(nil))
+	}
 	return a
+}
+
+// IsSingle ...
+func (a *Assign) IsSingle() bool {
+	return len(a.dst) == 1
 }
